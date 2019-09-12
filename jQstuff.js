cf9e81b83226e611b84a95ce9571c65b1c31a5b0
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-09-12 09:51:25 -0700

    Update jQstuff.js

--- jQstuff.js
+++ jQstuff.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-  $("q1").click(function(){
-    $("a1").toggle();
+  $("#q1").click(function(){
+    $("#a1").toggle();
   });
-  $("q2").click(function(){
-    $("a2").toggle();
+  $("#q2").click(function(){
+    $("#a2").toggle();
   });
-  $("q3").click(function(){
-    $("a3").toggle();
+  $("#q3").click(function(){
+    $("#a3").toggle();
   });
-  $("q4").click(function(){
-    $("a4").toggle();
+  $("#q4").click(function(){
+    $("#a4").toggle();
   });
-  $("q5").click(function(){
-    $("a5").toggle();
+  $("#q5").click(function(){
+    $("#a5").toggle();
   });
 });
